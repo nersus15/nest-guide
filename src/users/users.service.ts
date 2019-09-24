da81ae4d3ac7edc040d12dadc1849c862e70553e
nersus15 <fathur.ashter15@gmail>
2019-09-24 20:12:41 +0800

    membuat service (function pertama)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [];
+    getAllUsers() {
+        return this.users;
+    }
+}
