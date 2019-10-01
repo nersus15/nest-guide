c477b854cf24ff40b085f478800d1a0c6f13b171
nersus15 <fathur.ashter15@gmail>
2019-10-01 22:55:08 +0800

    menambahkan module auth beserta controller dan service nya

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [TypeOrmModule.forRoot(TypeOrmConfig),
-    UsersModule],
+    UsersModule,
+    AuthModule],
 })
 export class AppModule { }
